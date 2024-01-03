713f10e56c9449ac71784a3740baea24ca9649ac
youhan <yoye0001@stud.kea.dk>
2024-01-03 18:12:29 +0100

    firstcommit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenu = document.querySelector(".burger-menu");
+  const navLinks = document.querySelector("header nav ul");
+
+  burgerMenu.addEventListener("click", () => {
+    console.log("Burger clicked");
+    burgerMenu.classList.toggle("active");
+    navLinks.classList.toggle("show");
+  });
+
+  // Close the menu when a navigation item is clicked
+  document.querySelectorAll("header nav ul li a").forEach((item) => {
+    item.addEventListener("click", () => {
+      console.log("Link clicked");
+      burgerMenu.classList.remove("active");
+      navLinks.classList.remove("show");
+    });
+  });
+
+  // Close the menu when clicking outside the menu
+  document.addEventListener("click", (event) => {
+    const isClickInside = navLinks.contains(event.target) || burgerMenu.contains(event.target);
+
+    if (!isClickInside) {
+      console.log("Clicked outside menu");
+      burgerMenu.classList.remove("active");
+      navLinks.classList.remove("show");
+    }
+  });
+});
